83bb9ce2161353e4454a0798d43b37fc01c315eb
Celesian <insidetf2@gmail.com>
2023-02-12 11:26:55 -0300

    changed sentry/node to sentry/browser

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,19 +1,20 @@
-import * as Sentry from '@sentry/node';
-
-import crypto from 'crypto';
+import * as Sentry from '@sentry/browser';
  
 Sentry.init({
     dsn: "https://81d70e0b95b74fcfaedda89b8485097c@o460174.ingest.sentry.io/4504667394801664",
+    
+    tracesSampleRate: 1.0
   });
  
+
+
 /** @type {import('@sveltejs/kit').HandleServerError} */
 export function handleError({ error, event }) {
-  const errorId = crypto.randomUUID();
-  // example integration with https://sentry.io/
-  Sentry.captureException(error, { event, errorId });
- 
-  return {
+// example integration with https://sentry.io/
+Sentry.captureException(error, { event });
+
+return {
     message: 'Whoops!',
-    errorId
-  };
+    code: error?.code ?? 'UNKNOWN'
+};
 }
